d70307c8dbc964f87c4df10143bbd43a999c0c94
Maayan Glikser <maayan@glikm.com>
2017-08-01 22:44:14 +0300

    Release v1.4.4

--- dist/xhook.js
+++ dist/xhook.js
@@ -1,4 +1,4 @@
-// XHook - v1.4.3 - https://github.com/jpillora/xhook
+// XHook - v1.4.4 - https://github.com/jpillora/xhook
 // Jaime Pillora <dev@jpillora.com> - MIT Copyright 2017
 (function(undefined) {
 var AFTER, BEFORE, COMMON_EVENTS, EventEmitter, FETCH, FIRE, FormData, NativeFetch, NativeFormData, NativeXMLHttp, OFF, ON, READY_STATE, UPLOAD_EVENTS, WINDOW, XHookFetchRequest, XHookFormData, XHookHttpRequest, XMLHTTP, convertHeaders, depricatedProp, document, fakeEvent, mergeObjects, msie, proxyEvents, slice, useragent, xhook, _base,
