5ca3a35cc54e40ec318aaa7a3c02df68fedbdd14
TrickyPi <530257315@qq.com>
2022-07-24 12:03:59 +0800

    test: tweak test of fetch

--- tests/fetch/fetch-catch.spec.ts
+++ tests/fetch/fetch-catch.spec.ts
@@ -2,39 +2,32 @@ import { test, expect } from "@playwright/test";
 
 test("Should be reject if native fetch is reject", async ({ page }) => {
     await page.goto("http://127.0.0.1:8080/example/common.html");
-    const obj = {
-        isError: false,
-        isFinished: false
-    }
-    await page.evaluate((obj) => {
-        fetch('http://127.0.0.1:8080/unexsited').catch(() => {
-            obj.isError = true
-        }).finally(() => {
-            obj.isFinished = true
+    const callInCatch = await page.evaluate(async () => {
+        let callInCatch = false
+        await fetch('http://127.0.0.1:8081/unexsited').catch(() => {
+            callInCatch = true
         })
-    }, obj)
-    let setI = setInterval(() => {
-        if (obj.isFinished) {
-            expect(obj.isError).toEqual(true)
-            clearInterval(setI)
-        }
-    }, 100)
+        return callInCatch
+    })
+    expect(callInCatch).toEqual(true)
 });
-
-
 
 test("Should call the afterHook when a request fails", async ({ page }) => {
     await page.goto("http://127.0.0.1:8080/example/common.html");
-    const callInAfter = await page.evaluate(async () => {
+    const { callInAfter, isCallFailed } = await page.evaluate(async () => {
         let callInAfter = false
+        let isCallFailed = false
         //@ts-ignore
         xhook.after((req, res) => {
             callInAfter = true
         })
         try {
             await fetch('http://127.0.0.1:8081/unexsited')
-        } catch { }
-        return callInAfter
+        } catch {
+            isCallFailed = true
+        }
+        return { callInAfter, isCallFailed }
     })
+    expect(isCallFailed).toBe(true)
     expect(callInAfter).toBe(true)
 });
